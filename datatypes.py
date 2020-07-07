1d92c92464a901e64205396961df150203c4a936
Vikrant-Deshmukh <vikrantdeshmukh007@gmail.com>
2020-07-07 10:09:28 +0530

    Added new content for data types.

--- datatypes.py
+++ datatypes.py
@@ -10,6 +10,26 @@ print(type(num))
 #Exponent: 3 ** 2
 #Modulus: 3 % 2
 
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Tuple
+
 print(3 ** 2) #3 to the power 2
 print(3 % 2) #remainder of division.Can be used to find Even and odd numbers.
 
